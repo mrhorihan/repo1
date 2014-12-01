887f63a39824b1a8f40f7f232c277406ac2fbf69
Mike Horihan <mrhorihan@gmail.com>
2014-12-01 16:46:59 -0500

    test

---
+++ CommonLib/CommonLib/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommonLib
+{
+    public class AlphaClass
+    {
+        public string GetAlpha()
+        {
+            return "Alpha";
+        }
+    }
+}

b7f5d05fe4544d254e680d2bcd94c917d092367c
mrhorihan <mrhorihan@gmail.com>
2015-02-22 21:03:41 -0500

    testing

--- CommonLib/CommonLib/Class1.cs
+++ CommonLib/CommonLib/Class1.cs
@@ -17,14 +17,10 @@ namespace CommonLib
             return "Beta";
         }
 
-        public string Gamma()
-        {
-            return "Gamma";
-        }
+        //public string Gamma()
+        //{
+        //    return "Gamma";
+        //}
 
-        public string Delta()
-        {
-            return "Delta";
-        }
-    }
+   }
 }
